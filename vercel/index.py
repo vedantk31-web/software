c58c97b003dab9100d4c1f5ad281d915210de03f
vedantk31-web <146844325+vedantk31-web@users.noreply.github.com>
2024-06-30 13:58:41 +0530

    Update index.py

--- vercel/index.py
+++ vercel/index.py
@@ -1,1 +1,10 @@
-from wsgi import app
+import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from wsgi import app as flask_app
+
+def handler(event, context):
+    return flask_app(event, context)
